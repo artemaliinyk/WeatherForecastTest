46bd0bc9ef79134066d45ca930e91218d907e6c6
Artem <shotty.log@gmail.com>
2024-02-07 15:07:07 +0200

    Create weather table (migration)

---
+++ database/migrations/2024_02_07_130006_create_weather_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weather', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('timestamp_dt');
+            $table->string('city_name');
+            $table->decimal('min_tmp', 8, 2);
+            $table->decimal('max_tmp', 8, 2);
+            $table->decimal('wind_spd', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weather');
+    }
+};
