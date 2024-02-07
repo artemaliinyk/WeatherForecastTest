02dca2222e82a4546a8758c989d85a35a427dd60
Artem <shotty.log@gmail.com>
2024-02-07 15:09:03 +0200

    Create a model

---
+++ app/Models/Weather.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Weather extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'timestamp_dt',
+        'city_name',
+        'min_tmp',
+        'max_tmp',
+        'wind_spd'
+    ];
+}
